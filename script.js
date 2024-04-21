ef7ebdde3ef47309db79f59282d6d897f645049e
FES001 <firdavs7794948@gmail.com>
2024-04-21 23:02:03 +0500

    s

--- script.js
+++ script.js
@@ -20,3 +20,38 @@ cartButton.addEventListener('click', function() {
 
     
 });
+
+// ADD TO CART BUTTON AND PRICES
+
+const addToCartBtn = document.querySelector('.add-cart-button');
+let price = 125;
+
+/* PRICES */
+
+const minusNumber = document.getElementById('minusNumber');
+const plusNumber = document.getElementById('plusNumber');
+const numberOutput = document.getElementById('numberOutput');
+let number = 0;
+
+minusNumber.addEventListener('click', function() {
+
+    if (number > 0) {
+        number --;
+        numberOutput.innerText = number;
+    } else {
+        let minusNumberAlert = "Add a product please!"
+        alert(minusNumberAlert);
+    }
+
+})
+
+plusNumber.addEventListener('click', function() {
+
+    if (number < 10) {
+        number ++;
+        numberOutput.innerText = number;
+    } else {
+        alert("You have reached the maximum amount of shoes you can buy in once.")
+    }
+
+})
